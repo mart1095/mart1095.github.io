dfff473ebeb53968cf89a6ef00dfdda32c15cff8
mart1095 <mart1095@algonquinlive.com>
2018-04-25 15:25:59 -0400

    Added javascript, gallery page, about page, images, and image source text

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,2 +1,19 @@
-// This script file would have the js (Java) that is shared between all pages
-// i.e. Java for hamburger, loading animations
+window.onload = init;
+
+// hamburger menu
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+function showHideMobileMenu() {
+
+	var mobileNav = document.querySelector('.mobile-nav');
+
+	// in a if statement == is used to compare two value, if the values matches then the condition is true
+	// an if statement is followed by an else statement which runs when the given condition is not met
+	if(mobileNav.style.display=="block") {
+		mobileNav.style.display="none";
+	} else {
+		mobileNav.style.display="block";
+	}
+}
