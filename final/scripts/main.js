5330b3ca14c009a429ffa070a0290c0c91cdcb79
mart1095 <mart1095@algonquinlive.com>
2018-04-25 17:26:13 -0400

    hamburger js

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,19 +1,10 @@
 window.onload = init;
 
+function init () {
+
 // hamburger menu
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+$('.ham').click(function(){
+	$('.mobile-nav').slideToggle(500);
+});
 
-function showHideMobileMenu() {
-
-	var mobileNav = document.querySelector('.mobile-nav');
-
-	// in a if statement == is used to compare two value, if the values matches then the condition is true
-	// an if statement is followed by an else statement which runs when the given condition is not met
-	if(mobileNav.style.display=="block") {
-		mobileNav.style.display="none";
-	} else {
-		mobileNav.style.display="block";
-	}
 }
--- final/scripts/validation.js
+++ final/scripts/validation.js
@@ -1,1 +1,22 @@
 // The js required for the validation of contact form would go here
+window.onload = init;
+
+function init(){
+	$('submit').on('click', function(event){
+		event.preventDefault();
+
+		required_check();
+	});
+}
+
+function required_check(){
+	var required_inputs = $('.required input, .required textarea');
+}
+
+var required_checkbox_containers = $('.required-checkbox');
+
+
+
+function validate_input() {
+	
+}
