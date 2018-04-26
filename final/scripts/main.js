17f9da983da322ec7dd10ce463d22c1ad4242986
mart1095 <mart1095@algonquinlive.com>
2018-04-25 21:00:16 -0400

    last changes

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -7,4 +7,16 @@ $('.ham').click(function(){
 	$('.mobile-nav').slideToggle(500);
 });
 
+// slideshow
+if($('.image-gallery').length> 0 ) {
+
+		slider_init();
+	}
+
+// form
+if($('#htmlForm').length > 0 ) {
+
+		validation_init();
+	}
+
 }
--- final/scripts/validation.js
+++ final/scripts/validation.js
@@ -1,22 +1,67 @@
-// The js required for the validation of contact form would go here
 window.onload = init;
 
-function init(){
-	$('submit').on('click', function(event){
+function validation_init(){
+
+	$('#btn-submit').on('click', function(event){
 		event.preventDefault();
 
-		required_check();
+		var required_error = required_check();
+		var validate_error = validate_input();
+
+		if(!required_error && !validate_error) {
+			alert('Thank you for submitting a message!');
+		} else {
+			alert('Please fill in missing sections.');
+		}
 	});
 }
 
-function required_check(){
-	var required_inputs = $('.required input, .required textarea');
-}
-
-var required_checkbox_containers = $('.required-checkbox');
-
-
-
 function validate_input() {
-	
+
+	var validate_inputs = $('.validate input');
+	var validation_error = false;
+
+	$.each(validate_inputs, function(input_index, validate_input) {
+
+		var validate_container = $(validate_input).closest('.input-container');
+		var input_label = $(validate_container).find('.label-text').text();
+		var input_val = $(validate_input).val().trim();
+		var feedback = $(validate_container).find('.feedback');
+		var feedback_text = "";
+		var has_error = false;
+ 
+		if(input_val!='') {
+
+			$(feedback).fadeOut(0);
+
+			if(validation_type=='text') {
+
+			if(validation_type=='email') {
+				pattern = /(.+)@(.+){2,}\.(.+){2,}/;
+
+				if(!input_val.match(pattern)) {
+					feedback_text = 'Must use valid email. "'+input_label+'"';
+					has_error = true;
+
+				}
+			}
+
+			if(has_error) {
+				$(validate_container).removeClass('has-success');
+				$(validate_container).addClass('has-error');
+				validation_error = true;
+			} else {
+				$(validate_container).removeClass('has-error');
+				$(validate_container).addClass('has-success');
+				feedback_text = 'The entered value for "'+input_label+'" is acceptable.';
+ 			}
+
+			$(feedback).html(feedback_text);
+			$(feedback).fadeIn(300);
+		}
+
+	};
+
+	return validation_error;
+
 }
